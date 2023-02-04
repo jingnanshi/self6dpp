f89dafeab31bcc79c47d24ad43d761fe052e5af0
Jingnan Shi <jnshi@mit.edu>
2023-02-04 18:20:12 -0500

    add gt test configs

---
+++ configs/self6dpp-gt-test/ssYCBV/ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10/ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10_12_21BleachCleanser.py
@@ -0,0 +1,13 @@
+_base_ = "./ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10_01_02MasterChefCan.py"
+OUTPUT_DIR = (
+    "output/self6dpp/ssYCBV/ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10/12_21BleachCleanser"
+)
+DATASETS = dict(
+    TRAIN=("ycbv_021_bleach_cleanser_train_real_aligned_Kuw",),
+    TRAIN2=("ycbv_021_bleach_cleanser_train_pbr",),
+)
+MODEL = dict(
+    WEIGHTS="output/gdrn/ycbvPbrSO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO/12_21BleachCleanser/model_final_wo_optim-59a61f06.pth"
+)
+
+TEST = dict(EVAL_PERIOD=0, VIS=False, TEST_BBOX_TYPE="gt")  # gt | est
---
+++ configs/self6dpp-gt-test/ssYCBV/ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10/ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10_13_24Bowl.py
@@ -0,0 +1,11 @@
+_base_ = "./ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10_01_02MasterChefCan.py"
+OUTPUT_DIR = "output/self6dpp/ssYCBV/ss_mlBCE_MaskFull_PredDouble_PBR05_woCenter_edgeLower_refinePM10/13_24Bowl"
+DATASETS = dict(
+    TRAIN=("ycbv_024_bowl_train_real_aligned_Kuw",),
+    TRAIN2=("ycbv_024_bowl_train_pbr",),
+)
+MODEL = dict(
+    WEIGHTS="output/gdrn/ycbvPbrSO/resnest50d_AugCosyAAEGray_BG05_visib10_mlBCE_DoubleMask_ycbvPbr100e_SO/13_24Bowl/model_final_wo_optim-55b952fc.pth"
+)
+
+TEST = dict(EVAL_PERIOD=0, VIS=False, TEST_BBOX_TYPE="gt")  # gt | est
